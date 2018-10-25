a20964f73891274f992fb5e0c4b91dae85e1941a
jaromir.holoubek <jaromir.holoubek@zpa.cz>
2018-10-25 16:55:13 +0200

    Přidal jsem třídu ClassB.

---
+++ ConsoleApp_StandardHelloWorld/ClassB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_dHelloWorld_Standar
+{
+    class ClassB
+    {
+        public ClassB()
+        {
+
+        }
+
+
+        public string DefaultString
+        {
+            get
+            {
+                return "BBBB";
+            }
+        }
+    }
+}
